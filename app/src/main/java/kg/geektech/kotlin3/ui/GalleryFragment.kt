15d8e6b34b8312c52d67c30743f4b7ba285b6caf
Kochevnikbbb <bbelekov@mail.ru>
2022-04-28 19:00:32 +0600

    еще не доделал

---
+++ app/src/main/java/kg/geektech/kotlin3/ui/GalleryFragment.kt
@@ -0,0 +1,12 @@
+package kg.geektech.kotlin3.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kg.geektech.kotlin3.R
+
+class GalleryFragment : Fragment() {
+
+}
